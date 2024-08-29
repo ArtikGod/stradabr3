489e7047383b9288d741cf18d5f9f57136007fd9
ArtikGod <166165069+ArtikGod@users.noreply.github.com>
2024-08-29 16:16:40 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-consol.log('hi');
+console.log('hello github');
+console.log('worked');
+
+function culc (operation, a, b) {
+    if (operation === 'add') {
+        return a + b;
+} else if (operation === 'multi') {
+    return a * b;
+} else if (operation ==='subtact') {
+    return a - b;
+}
+}
+console.log(culc('add',1,2))
+console.log(culc('multi',5,6))
