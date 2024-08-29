b50492d8101704a1d3a74afd415400a9c440a7c9
ArtikGod <166165069+ArtikGod@users.noreply.github.com>
2024-08-29 16:06:16 +0300

    Update main.js
    wow

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-console.log('hello');
-console.log('hello');
-console.log('hello');
-console.log('hello');
+console.log('hello github');
+console.log('worked');
+
+function culc (operation, a, b) {
+    if (operation === 'add') {
+        return a + b;
+} else if (operation === 'multi') {
+    return a * b;
+} else if (operation ==='subtact') {
+    return a - b;
+}
+}
+console.log(culc('add',1,2))
